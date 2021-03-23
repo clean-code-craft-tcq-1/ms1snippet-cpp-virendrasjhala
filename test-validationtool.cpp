b4405b458831f8162ac707f59c06449ff814933d
virendra <16984895+virendrasjhala@users.noreply.github.com>
2021-03-23 12:26:08 +0530

    test5.1

--- test-validationtool.cpp
+++ test-validationtool.cpp
@@ -23,11 +23,11 @@ TEST_CASE("reports error when current jumps abruptly") {
 	double currentReadings[] = { 0.03, 0.03, 0.03, 0.33 };
 	int numOfCurReadings = sizeof(currentReadings) / sizeof(currentReadings[0]);
 
-	Batterty_Soc c_parameter;
-	REQUIRE(c_parameter.isParameterValid(currentReadings) == true);
+	//Batterty_Soc c_parameter;
+	//REQUIRE(c_parameter.isParameterValid(currentReadings) == true);
        
-	//Batterty_Soc Batterty_Soc;
-	//REQUIRE(Batterty_Soc.validateCurrentreadings(currentReadings, numOfCurReadings) == false);
+	Batterty_Soc Batterty_Soc;
+	REQUIRE(Batterty_Soc.validateCurrentreadings(currentReadings, numOfCurReadings) == false);
 	//system("pause");
 
 }
